8ee8e8f72a33ff68cae5cf162def51407345b051
joanansia9 <you@example.com>
2018-04-02 00:33:17 +0200

    Colors & images setup -> Login and registration panels.

--- app/src/main/java/com/puszek/jm/puszek/RegisterUserActivity.java
+++ app/src/main/java/com/puszek/jm/puszek/RegisterUserActivity.java
@@ -8,6 +8,8 @@ import android.widget.Button;
 import android.widget.EdgeEffect;
 import android.widget.EditText;
 
+import com.puszek.jm.puszek.helpers.FieldsValidator;
+
 import uk.co.chrisjenx.calligraphy.CalligraphyContextWrapper;
 
 public class RegisterUserActivity extends AppCompatActivity {
@@ -15,17 +17,24 @@ public class RegisterUserActivity extends AppCompatActivity {
     EditText password;
     EditText repeatedPassword;
     EditText street;
+    EditText city;
+    EditText hNumber;
     Button registerUserButton;
+    FieldsValidator validator;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register_user);
+
+        validator = new FieldsValidator(this);
         login = findViewById(R.id.regLoginEt);
         password = findViewById(R.id.regPasswordEt);
         repeatedPassword = findViewById(R.id.regRepPasswordEt);
         street = findViewById(R.id.regStreetEt);
+        city = findViewById(R.id.city);
+        hNumber = findViewById(R.id.houseNumber);
 
         registerUserButton = findViewById(R.id.regUserButton);
     }
@@ -36,5 +45,10 @@ public class RegisterUserActivity extends AppCompatActivity {
     }
 
     public void registerUser(View view) {
+        if(validator.isValidField(login)&&validator.isValidPassword(password,repeatedPassword)
+                && validator.isValidField(city) &&validator.isValidField(street)&&validator.isValidHouseNumber(hNumber)){
+            //register user at database
+            System.out.println("HURRAAAA");
+        }
     }
 }
--- app/src/main/java/com/puszek/jm/puszek/helpers/FieldsValidator.java
+++ app/src/main/java/com/puszek/jm/puszek/helpers/FieldsValidator.java
@@ -1,24 +1,61 @@
 package com.puszek.jm.puszek.helpers;
 
+import android.content.Context;
+import android.widget.EditText;
+import android.widget.TextClock;
 import android.widget.TextView;
 
+import com.puszek.jm.puszek.R;
+
+import org.w3c.dom.Text;
+
 import java.lang.reflect.Field;
+import java.util.Objects;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * Created by joannamaciak on 29/03/2018.
  */
 
 public class FieldsValidator {
-    public FieldsValidator(){
+    private Context context;
 
-    }
-    public boolean isValidLogin(TextView login){
-
-        return true;
+    public FieldsValidator(Context c) {
+        this.context = c;
     }
 
-    public boolean isValidPassword(TextView password){
-
-        return true;
+    public boolean isValidField(EditText field) {
+        String log = field.getText().toString().trim();
+        if (log.equals("")) {
+            field.setError(context.getString(R.string.required));
+            return false;
+        } else return true;
     }
+
+    public boolean isValidPassword(EditText p1, EditText p2) {
+        if (p1.getText().toString().trim().length() < 6) {
+            p1.setError(context.getString(R.string.min_signs_value));
+            return false;
+        } else if (!Objects.equals(p1.getText().toString(), p2.getText().toString())) {
+            p1.setError(context.getString(R.string.identity_required));
+            p2.setText("");
+            return false;
+        } else return true;
+    }
+
+    public boolean isValidHouseNumber(EditText houseNumber) {
+        if (!isValidField(houseNumber)) return false;
+        else {
+            String ADDRESS_PATTERN = "^[0-9-\\+]*$";
+            Pattern pattern = Pattern.compile(ADDRESS_PATTERN);
+
+            if (!pattern.matcher(houseNumber.getText().toString()).matches()) {
+                houseNumber.setError(context.getString(R.string.incorrect_format));
+                return false;
+            } else return true;
+        }
+
+    }
+
 }
